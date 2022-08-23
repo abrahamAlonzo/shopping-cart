81713143e65d5f650c63c6e4150322cd830d304c
davidalan004 <108294908+davidalan004@users.noreply.github.com>
2022-08-22 22:43:53 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Blogs from './components/Blogs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    blogs:[
+     { articulo:'Is it good to consolidate debt?', fecha:'January 5, 2022', img:'/fotos/articulo1.jpg' },
+     { articulo:'Is it good to consolidate debt?', fecha:'January 3, 2022', img:'/fotos/articulo2.jpg' },
+     { articulo:'Is it good to consolidate debt?', fecha:'January 1, 2022', img:'/fotos/articulo3.jpg' },
+     { articulo:'Is it good to consolidate debt?', fecha:'December 27, 2021', img:'/fotos/articulo4.jpg' },
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Blogs 
+          blogs={this.state.blogs}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
