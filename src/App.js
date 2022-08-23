4bf55108da3d066ae87966da2487131e3e8daf8d
davidalan004 <108294908+davidalan004@users.noreply.github.com>
2022-08-23 11:53:13 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { Component } from 'react';
-import Blogs from './components/Blogs'
-import Layout from './components/Layout'
+import Blogs from './components/Blogs';
+import Layout from './components/Layout';
+import Title from './components/Title';
 
 
 class App extends Component {
   state = {
     blogs:[
-     { articulo:'Is it good to consolidate debt?', fecha:'January 5, 2022', img:'/fotos/articulo1.jpg' },
-     { articulo:'Is it good to consolidate debt?', fecha:'January 3, 2022', img:'/fotos/articulo2.jpg' },
-     { articulo:'Is it good to consolidate debt?', fecha:'January 1, 2022', img:'/fotos/articulo3.jpg' },
-     { articulo:'Is it good to consolidate debt?', fecha:'December 27, 2021', img:'/fotos/articulo4.jpg' },
+     { article:'Is it good to consolidate debt?', date:'January 5, 2022', img:'/fotos/articulo1.jpg' },
+     { article:'Is it good to consolidate debt?', date:'January 3, 2022', img:'/fotos/articulo2.jpg' },
+     { article:'Is it good to consolidate debt?', date:'January 1, 2022', img:'/fotos/articulo3.jpg' },
+     { article:'Is it good to consolidate debt?', date:'December 27, 2021', img:'/fotos/articulo4.jpg' },
     ]
   }
   render() {
     return (
       <div>
         <Layout>
+          <Title />
           <Blogs 
              blogs={this.state.blogs}
              />
