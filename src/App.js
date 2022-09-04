08c56abde711397d1ade03c0fdb7d4062381126e
davidalan004 <davidmxl_004@hotmail.com>
2022-09-04 10:20:54 -0700

    Create folder structure of scalability of the project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Blogs from './components/Blogs';
 import Layout from './components/Layout';
 import Title from './components/Title';
 
-
 class App extends Component {
   state = {
     blogs:[
-     { article:'Is it good to consolidate debt?', date:'January 5, 2022', img:'/fotos/articulo1.jpg' },
-     { article:'Is it good to consolidate debt?', date:'January 3, 2022', img:'/fotos/articulo2.jpg' },
-     { article:'Is it good to consolidate debt?', date:'January 1, 2022', img:'/fotos/articulo3.jpg' },
-     { article:'Is it good to consolidate debt?', date:'December 27, 2021', img:'/fotos/articulo4.jpg' },
+     { article:'Is it good to consolidate debt?', date:'January 5, 2022', img:'/assets/image/article1.jpg' },
+     { article:'Is it good to consolidate debt?', date:'January 3, 2022', img:'/assets/image/article2.jpg' },
+     { article:'Is it good to consolidate debt?', date:'January 1, 2022', img:'/assets/image/article3.jpg' },
+     { article:'Is it good to consolidate debt?', date:'December 27, 2021', img:'/assets/image/article4.jpg' },
     ]
   }
   render() {
