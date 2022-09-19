ae524a4cd3160bbfab398ed11314d8ec2324d1d8
davidalan004 <davidmxl_004@hotmail.com>
2022-09-18 18:38:32 -0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Blog from './layout/Blog.jsx';
+import { Suspense } from 'react';
+
+
+ function App() {
+  return (
+      //<Suspense fallback={null}>
+        <Blog />
+      //</Suspense>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import i18next from 'i18next';
+import en from "./localization/en/en.json";
+import es from "./localization/es/es.json";
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18next
+ .use(initReactI18next)
+ .use(LanguageDetector)
+ .init({
+  interpolation: {escapeValue: false},
+  lng: "en",
+  debug: true,
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      localization: en,
+    },
+    es: {
+      localization: es,
+    },
+  },
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <I18nextProvider i18n={i18next}>
+    <React.StrictMode>    
+        <App />
+    </React.StrictMode>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
